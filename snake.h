be567b2deb64598a64bb2bd9e6aa6a48235c0e22
Mia2b <Mia2b@hotmail.com>
2018-11-20 11:06:29 -0700

    setting up the snake object

---
+++ snake.h
@@ -0,0 +1,13 @@
+#ifndef snake_h
+#define snake_h
+
+class snake
+{
+    private:
+        uint8_t x, y;
+        uint8_t lastX, lastY;
+        Snake child;
+        bool hasChild;
+    public:
+};
+#endif
